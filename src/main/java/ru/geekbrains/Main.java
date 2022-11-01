43aa789eeced695d7a03bd02a5abcefe131eed5e
FleaPepper <cathudson@yandex.ru>
2022-11-01 16:12:24 +0300

    commit

--- src/main/java/ru/geekbrains/Main.java
+++ src/main/java/ru/geekbrains/Main.java
@@ -1,5 +1,17 @@
 package ru.geekbrains;
 
+import org.junit.Assert;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Assert.assertEquals("", func());
+
+    }
+
+    public static String func() {
+        return "";
+    }
 
 }
